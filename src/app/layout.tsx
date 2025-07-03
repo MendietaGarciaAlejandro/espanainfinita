3f328c85ba96b383f431f25621738745e947ed2b
EnzoLabriego <153531705+MendietaGarciaAlejandro@users.noreply.github.com>
2025-07-03 17:54:44 +0200

    refactor(layout): elimina importación del componente NavMenu y ajusta la estructura del layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import NavMenu from "./components/NavMenu";
 import { Header } from "./components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
